f77b8d94c46fd1d740384ca8f74a4c13ec53e7a4
abusamin44 <abusamin44@gmail.com>
2026-04-28 14:42:26 +0600

    again try

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -1,5 +1,5 @@
 const BACKEND_URL = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' 
-    ? 'http://localhost:5000/api' 
+    ? 'http://localhost:5001/api' 
     : '/api'; 
 
 window.API_URL = BACKEND_URL;
